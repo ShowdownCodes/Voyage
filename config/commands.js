27b6a59822e9e59d178929e7e1f7eceb8b4822c5
ShowdownCodes <ShowdownCodes@users.noreply.github.com>
2014-03-01 17:31:30 +0500

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1403,6 +1403,35 @@ var commands = exports.commands = {
 	 * Fun commands
 	 *********************************************************/
 	
+	derp: 'derpcannon',
+	dcannon: 'derpcannon',
+	cannon: 'derpcannon',
+	derpcannon: function(target, room, user) {
+		if (!target) return this.parse('/help derpcannon');
+
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser) {
+			return this.sendReply('User '+this.targetUsername+' not found.');
+		}
+		if (!this.can('mute', targetUser, room)) return false;
+		if (targetUser.mutedRooms[room.id] || targetUser.locked || !targetUser.connected) {
+			var problem = ' but was already '+(!targetUser.connected ? 'offline' : targetUser.locked ? 'locked' : 'muted');
+			if (!target) {
+				return this.privateModCommand('('+targetUser.name+' would be shooted by '+user.name+' \'s Derp Cannon' +problem+'.)');
+			}
+			return this.addModCommand(''+targetUser.name+' would be shooted by '+user.name+' \'s Derp Cannon' +problem+'.' + (target ? " (" + target + ")" : ""));
+		}
+
+		targetUser.popup(user.name+' has shooted you with their Derp Cannon! '+target);
+		this.addModCommand(targetUser.name+' was blasted by '+user.name+' \'s Derp Cannon!' + (target ? " (" + target + ")" : ""));
+		var alts = targetUser.getAlts();
+		if (alts.length) this.addModCommand(""+targetUser.name+"'s alts were also shot by the Derp Cannon: "+alts.join(", "));
+		this.add('|unlink|' + targetUser.userid);
+
+		targetUser.mute(room.id, 7*60*1000);
+	},
+	
 	ddproom: function(target, room, user) {
 		return this.parse("/join dundealsplace")
 	},
